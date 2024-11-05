34ce6b51c4987a801f4e69af47dbcac629504e54
Sharvi Ghogale <71582221+SharviGhogale@users.noreply.github.com>
2024-11-05 09:28:36 -0500

    New features added
    I have add the buyerAccountPage which adds funds and reviews the buyer profile details. I have also made chages to the global.css according to the buyerAccountPage. In page.tsx added a line to call the buyerAccountPage.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
 'use client'
 import React from 'react';
-import AddItem from './add_item'; // Import the AddItem component
+import FetchItemsComponent from './add_review_items';
+import BuyerAccountPage from './buyerAccountPage';
 
 export default function Page() {
   return (
-    <div>
-      <h1>Main Page</h1>
+    <div style={{ textAlign: 'center', padding: '20px' }}>
+      <img 
+        src="https://raw.githubusercontent.com/brianEtrials/team-bars-lisp-auction-house/d8cd3cc21d5bbe1940b592642c33f3242bf75fb4/img/logo.png" 
+        alt="Auction House Logo" 
+        style={{ width: '50px', height: 'auto' }}
+      />
 
-      <AddItem />
+      <FetchItemsComponent />
+      {/* Comment the <FetchItemsComponent/> so as to just see the BuyerAccountPage*/}
+      {/* If u uncomment the FetchItemsComponent and BuyerAccountPage will both be displayed on the same page */}
+      <BuyerAccountPage /> 
     </div>
   );
 }
