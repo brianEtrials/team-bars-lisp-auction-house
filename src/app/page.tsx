a40b095bc8733a5e69153a8d46ba2046ee400906
Antonela Tamagnini <antonelatamagnini@gmail.com>
2024-11-04 21:40:39 -0500

    logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,12 @@ import FetchItemsComponent from './add_review_items';
 
 export default function Page() {
   return (
-    <div>
-      <h1>AUCTION HOUSE</h1>
+    <div style={{ textAlign: 'center', padding: '20px' }}>
+      <img 
+        src="https://raw.githubusercontent.com/brianEtrials/team-bars-lisp-auction-house/d8cd3cc21d5bbe1940b592642c33f3242bf75fb4/img/logo.png" 
+        alt="Auction House Logo" 
+        style={{ width: '50px', height: 'auto' }}
+      />
 
       <FetchItemsComponent />
     </div>
