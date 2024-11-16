7810f262744a0d51a34e1f8dcc101dccf3098ac8
Brian Rojas <80777051+brianEtrials@users.noreply.github.com>
2024-11-16 17:32:25 -0500

    lambda expects idaccounts not just id

--- src/app/pages/closeaccounts.tsx
+++ src/app/pages/closeaccounts.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 interface UserId {
   id?: number;
 }
 
 const CloseAccount: React.FC<UserId> = ({ id }) => {
+  const navigate = useNavigate();
   const handleCloseAccount = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault(); // Prevent the default button action
     try {
@@ -13,10 +15,11 @@ const CloseAccount: React.FC<UserId> = ({ id }) => {
         "https://dyqqbfiore.execute-api.us-east-1.amazonaws.com/closeAccount/close",
         {
           headers: { "Content-Type": "application/json" },
-          data: id,
+          data: { idaccounts: id },
         }
       );
       console.log(response.data);
+      navigate("/");
     } catch (error) {
       console.error("Error during close account:", error);
     }
