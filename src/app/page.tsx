469dd9a16123eaf754ecb2a9d27b4fe22fe1e379
Antonela Tamagnini <antonelatamagnini@gmail.com>
2024-11-20 21:39:37 -0500

    Freeze-Unfreeze-Items
    -Admin Login
    -Freeze item
    -Unfreeze item

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,9 @@ import BuyerAccountPage from './pages/buyerAccountPage';
 import FetchItemsComponent from './pages/seller';
 import CutomerPage from './pages/customer';
 import ItemDetail from './pages/itemDetail';
+import AdminDashboard from  './pages/adminDashboard';
+import AdminLogin from './pages/adminLogin';
+import ManageItems from './pages/manageItems';
 
 export default function Page() {
   const [isClient, setIsClient] = useState(false);
@@ -30,6 +33,10 @@ export default function Page() {
         <Route path="/accounts" element={<Accounts />} />
         <Route path="/buyerAccountPage" element={<BuyerAccountPage />} />
         <Route path="/add_review_items" element={<FetchItemsComponent />} />
+        <Route path="/adminLogin" element={<AdminLogin />} />
+        <Route path="/adminDashboard" element={<AdminDashboard />} />
+        <Route path="/manageItems" element={<ManageItems />} />
+    
       </Routes>
     </BrowserRouter>
   );
