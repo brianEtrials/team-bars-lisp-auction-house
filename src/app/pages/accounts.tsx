833b01145fd4e6ae255d63d56f506d696bf3fadf
Sharvi Ghogale <71582221+SharviGhogale@users.noreply.github.com>
2024-12-13 16:18:01 -0500

    Added Brian's changes to rohan's branch
    changes inside account,buyerProfilePage,Seller

--- src/app/pages/accounts.tsx
+++ src/app/pages/accounts.tsx
@@ -115,6 +115,7 @@ export default function Accounts() {
       const accountInfo = typeof response.data.body === "string" ? JSON.parse(response.data.body) : response.data.body;
 
       secureLocalStorage.setItem("userCredentials", {
+        id: accountInfo.idaccounts,
         username: accountInfo.username,
         account_type: accountInfo.account_type,
       });
